82a5976582a7e2c2ce22a0a780d895e885bf34ae
José Guilherme <guim22771@gmail.com>
2025-10-05 23:39:27 -0300

    feat: auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { CacheKeysEnum } from "./types";
+
+const PRIVATE_ROUTES = ["/tasks"];
+
+const PUBLIC_ROUTES = ["/login"];
+
+export function middleware(request: NextRequest) {
+  const user = request.cookies.get(CacheKeysEnum.USER)?.value;
+  const url = request.nextUrl.clone();
+  const { pathname } = url;
+
+  if (!user && PRIVATE_ROUTES.some((route) => pathname.startsWith(route))) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (user && PUBLIC_ROUTES.includes(pathname)) {
+    return NextResponse.redirect(new URL("/tasks", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/tasks/:path*", "/login"],
+};
